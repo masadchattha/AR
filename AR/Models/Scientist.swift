fe8b0399ba15382cc338c0616d1bd00309550a9f
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-01-07 17:39:04 +0500

    feat: add "scientists" JSON file and Modal

---
+++ AR/Models/Scientist.swift
@@ -0,0 +1,17 @@
+//
+//  Scientist.swift
+//  AR
+//
+//  Created by Muhammad Asad Chattha on 07/01/2024.
+//
+
+import Foundation
+
+struct Scientist: Decodable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
